ea9979ff4da5dfc8b92ca0165824e9cc39baff24
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-04-13 15:21:53 +0530

    Revert "terraform files added"
    This reverts commit 6a2f454667059cf2a2e76b93933d94cbe8391a19.

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {}
-variable "cluster_name" {}
-variable "docker_image" {}
-variable "cpu" { default = "256" }
-variable "memory" { default = "512" }
-variable "log_group" {}
-variable "subnets" {
-  type = list(string)
-}
